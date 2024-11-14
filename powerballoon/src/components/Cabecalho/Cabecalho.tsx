35a00270adf5bc74c828b668bc6e1cce0fcf5109
CaiocrNyimi <caiocrnyimi@gmail.com>
2024-11-13 22:36:13 -0300

    Fazendo o Boyler Plate do projeto

---
+++ powerballoon/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,10 @@
+import Menu from "../Menu/Menu";
+
+export default function Cabecalho(){
+    return(
+        <header>
+            <h1 className="cabecalho">Cabecalho</h1>
+            <Menu/>
+        </header>
+    )
+}
---
+++ powerballoon/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Rodape(){
+    return(
+        <div>
+            <h2>Rodapé</h2>
+        </div>
+    )
+}
